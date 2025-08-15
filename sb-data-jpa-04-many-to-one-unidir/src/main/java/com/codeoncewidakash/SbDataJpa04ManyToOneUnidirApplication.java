e87701b4e1faabf0ea5db1f1de97216078e100c4
codeonce-with-akash <akashodwa4@gmail.com>
2025-08-15 22:08:48 +0530

    Added the support of fetching dependencies from Nexus repository

---
+++ sb-data-jpa-04-many-to-one-unidir/src/main/java/com/codeoncewidakash/SbDataJpa04ManyToOneUnidirApplication.java
@@ -0,0 +1,13 @@
+package com.codeoncewidakash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDataJpa04ManyToOneUnidirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbDataJpa04ManyToOneUnidirApplication.class, args);
+	}
+
+}
